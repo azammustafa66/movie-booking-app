4016417077083e520fb52b7c9dfa9085ec813ede
Azam Mustufa <azammustafa66@gmail.com>
2026-09-17 00:06:42 +0530

    created catalog and booking services

---
+++ booking-service/src/test/java/demo/bookingservice/BookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.bookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
